7228db217c4efb55f71cc6d40134afe1e0927a5a
scaryboy806 <92325936+scaryboy806@users.noreply.github.com>
2023-10-25 10:51:54 +0200

    Saves
    Added saves system

--- src/App.java
+++ src/App.java
@@ -1,38 +1,67 @@
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Random;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         int tempSeznam[][] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
         int seznam[][] = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
-        boolean konec, loop, razlicen, veljavenVnos;
+        boolean konec, loop, razlicen, veljavenVnos, random;
+        random = false;
         konec = false;
         File data = new File("data.txt");
         Scanner vnos = new Scanner(System.in);
         Random rand = new Random();
         int x, y, z, vred;
-        String ukaz = "";
-        x = rand.nextInt(4);
-        y = rand.nextInt(4);
+        x = 0;
+        y = 0;
         vred = 2;
-        seznam[y][x] = vred;
-        loop = true;
-        System.out.println("Zelis naloziti zadnje shranjeno polje? Y/N");
+        String save;
+        String ukaz = "";
+        System.out.println("Zelis naloziti zadnje shranjeno polje? y/n");
         veljavenVnos = true;
         while (veljavenVnos) {
             ukaz = vnos.nextLine();
             if (ukaz.equals("y")) {
+                Scanner rf = new Scanner(data);
+                save = rf.nextLine();
+                rf.close();
+                if (!save.equals("-1")) {
+                    String[] saveArray = save.split(";");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 4; j++) {
+                            seznam[i][j] = Integer.parseInt(saveArray[4*i+j]);
+                        }
+                    }
+                }
+                else {
+                    random = true;
+                }
+                veljavenVnos = false;
+            }
+            else if (ukaz.equals("n")) {
+                random = true;
+                veljavenVnos = false;
+            }
+            else {
+                System.out.println("moznosti sta le y ali n");
             }
         }
-        while (loop) {
+        if (random) {
             x = rand.nextInt(4);
             y = rand.nextInt(4);
-            if (seznam[y][x] == 0) {
-                loop = false;
+            vred = 2;
+            seznam[y][x] = vred;
+            loop = true;
+            while (loop) {
+                x = rand.nextInt(4);
+                y = rand.nextInt(4);
+                if (seznam[y][x] == 0) {
+                    loop = false;
+                }
             }
+            seznam[y][x] = vred;
         }
-        seznam[y][x] = vred;
         for (int i = 0; i < 4; i++) {
             System.out.println();
             for (int j = 0; j < 4; j++) {
@@ -45,7 +74,6 @@ public class App {
             ukaz = vnos.nextLine();
             
             if (ukaz.equals("end")) {
-                vnos.close();
                 konec = true;
             }
             else {
@@ -219,7 +247,9 @@ public class App {
                         loop = false;
                     }
                 }
-                seznam[y][x] = vred;
+                if (razlicen) {
+                    seznam[y][x] = vred;
+                }
                 for (int i = 0; i < 4; i++) {
                     System.out.println();
                     for (int j = 0; j < 4; j++) {
@@ -235,6 +265,29 @@ public class App {
         } else {
             System.out.println("File already exists.");
         }
-        System.out.println("Zelis shraniti trenutno polje? (drugace bo shranjeno prazno polje)");
+        System.out.println("Zelis shraniti trenutno polje? (drugace bo shranjeno prazno polje) y/n");
+        ukaz = vnos.nextLine();
+        veljavenVnos = true;
+        while (veljavenVnos)
+            if (ukaz.equals("y")) {
+                FileWriter fw = new FileWriter(data);
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        fw.write(seznam[i][j]+";");
+                    }
+                }
+                fw.close();
+                veljavenVnos = false;
+            }
+            else if (ukaz.equals("n")) {
+                FileWriter fw = new FileWriter(data);
+                fw.write("-1");
+                fw.close();
+                veljavenVnos = false;
+            }
+            else {
+                System.out.println("Mozna vnosa sta le y in n");
+            }
+        vnos.close();
     }
 }
